e9b478bdd59d7a9679f76e1f51ff63ad4272a1e2
Rohit Verma <vv9002832@gmail.com>
2024-01-20 19:03:10 +0530

    commit

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import streamlit as st
 import base64
 
 def main():
-    st.set_page_config(page_title="niddydata", layout='wide')
+    st.set_page_config(
+        page_title="niddydata",
+        layout='wide',
+        page_icon="®️",
+    )
     cu1,cu2=st.columns(2)
     with cu1:
         st.markdown("# Hello👨🏻‍💻, I am Rohit")
@@ -69,57 +73,180 @@ def main():
         # Education section
         st.markdown("<h2 style='color: green;'>Education</h2>", unsafe_allow_html=True)
         st.write("I pursued my education at Devi Ahilya University in Indore, M.P.")
-        st.write("• Master of Science, Data Science (SGPA: 7.17)")
+        st.markdown("• Master of Science, Data Science (SGPA: <span style='color: red;'>7.17</span>)",unsafe_allow_html=True)
         st.write("  Post Graduation Date: May 2023")
-        st.write("• Bachelor of Science, Computer Science (SGPA: 6.8)")
+        st.markdown("• Bachelor of Science, Computer Science (SGPA: <span style='color: red;'> 6.8</span>)",unsafe_allow_html=True)
         st.write("  Graduation Date: Sep 2021")
+
+
 
         # Social activity section
         st.markdown("<h2 style='color: blue;'>Social Activity</h2>", unsafe_allow_html=True)
         st.write(
             "During my time at Devi Ahilya Vishwavidvalaya, I was involved in event organization and various social activities.")
 
-        # Contact information
-        st.markdown("<h2 style='color: purple;'>Get in Touch</h2>", unsafe_allow_html=True)
-        st.write("Feel free to connect with me via the following channels:")
-        st.write("- Email: your.email@example.com")
-        st.write("- LinkedIn: [Your LinkedIn Profile URL]")
-        st.write("- GitHub: [Your GitHub Profile URL]")
+# contect infomation
+
+        st.markdown("""
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+        """, unsafe_allow_html=True)
+    #;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+        st.markdown("""
+            <div style="text-align: center;">
+                <h2 style="color: purple;">Get in Touch</h2>
+                <p>Feel free to connect with me via the following channels:</p>
+        """, unsafe_allow_html=True)
+        first,second,third = st.columns(3)
+        with first:
+            st.markdown(
+                "[![Email](https://img.shields.io/badge/Email-rohitverma-red?style=for-the-badge&logo=gmail)](mailto:rohitvermav0021@gmail.com)")
+        with second:
+            st.markdown(
+                "[![LinkedIn](https://img.shields.io/badge/LinkedIn-Rohit%20Verma-blue?style=for-the-badge&logo=linkedin)](https://www.linkedin.com/in/rohit-verma-3094b8224/)")
+
+        with third:
+            st.markdown(
+                "[![GitHub](https://img.shields.io/badge/GitHub-RohitVerma0021-black?style=for-the-badge&logo=github)](https://github.com/RohitVerma0021)")
+
+
+
+
+    #=================================================================================================
     elif page == "Skills":
-        skill1,skill2=st.columns(2)
+        skill1, skill2 = st.columns(2)
+
         with skill1:
-            st.header("Skills")
-            st.write("Here are some of my key skills:")
-            st.write("- Python")
-            st.write("- MySQL")
-            st.write("- Database Managment")
-            st.write("- Machine Learning")
-            st.write("- Statistics")
-            st.write("- Natrual Language Processing")
-            st.write("- Power BI")
-            st.write("- Tableau")
-            st.write("- Web Scrapping")
-            st.write("- Data Analytics")
-            st.write("- Data Visualization")
-            st.write("- NumPy")
-            st.write("- Pandas")
-            st.write("- Plotly")
-            # Add more skills as needed
+            st.header("Data Science Skills")
+            st.markdown(
+                """
+                <div style="font-size: 18px; color: #2C3E50; font-weight: bold;">Key Data Science Skills:</div>
+                <ul style="list-style-type: none; padding-left: 0;">
+                    <li style="margin-bottom: 15px;">
+                        <b>Python:</b> Proficient in Python programming for data analysis, machine learning, and scripting.
+                    </li>
+                    <li style="margin-bottom: 15px;">
+                        <b>R:</b> Knowledge of R programming for statistical analysis and data visualization.
+                    </li>
+                    <li style="margin-bottom: 15px;">
+                        <b>NumPy:</b> Used for numerical operations and efficient array manipulations in Python.
+                    </li>
+                    <li style="margin-bottom: 15px;">
+                        <b>Pandas:</b> Expertise in data manipulation, analysis, and cleaning using the Pandas library.
+                    </li>
+                    <li style="margin-bottom: 15px;">
+                        <b>Matplotlib:</b> Created static, animated, and interactive visualizations with Matplotlib.
+                    </li>
+                    <li style="margin-bottom: 15px;">
+                        <b>Seaborn:</b> Employed for statistical data visualization, enhancing Matplotlib plots.
+                    </li>
+                    <li style="margin-bottom: 15px;">
+                        <b>Plotly:</b> Developed interactive and dynamic visualizations with Plotly in Python.
+                    </li>
+                </ul>
+                """, unsafe_allow_html=True
+                )
+
+            st.markdown(
+                    """
+                <div style="font-size: 18px; color: #2C3E50; font-weight: bold;">Advance Skills:</div>
+                <ul style="list-style-type: none; padding-left: 0;">
+                    <li style="margin-bottom: 15px;">
+                    <ul style="list-style-type: none; padding-left: 0;">
+                        <li style="margin-bottom: 15px;">
+                            <b>Machine Learning:</b> Implemented machine learning algorithms for predictive modeling.
+                        </li>
+                        <li style="margin-bottom: 15px;">
+                            <b>Scikit-Learn:</b> Utilized for machine learning tasks, including classification and regression.
+                        </li>
+                        <li style="margin-bottom: 15px;">
+                            <b>TensorFlow:</b> Developed deep learning models for complex tasks using TensorFlow.
+                        </li>
+                        <li style="margin-bottom: 15px;">
+                            <b>PyTorch:</b> Applied for deep learning and neural network implementations.
+                        </li>
+                        <li style="margin-bottom: 15px;">
+                            <b>Natural Language Processing (NLP):</b> Applied NLP techniques for text analysis and processing.
+                        </li>
+                        <li style="margin-bottom: 15px;">
+                            <b>Feature Engineering:</b> Created meaningful features to improve model performance.
+                        </li>
+                        <li style="margin-bottom: 15px;">
+                            <b>Model Evaluation and Hyperparameter Tuning:</b> Employed techniques to optimize model performance.
+                        </li>
+                    </ul>
+                    """, unsafe_allow_html=True
+                )
+
+
+            st.markdown(
+                """
+                <div style="font-size: 18px; color: #2C3E50; font-weight: bold;">Additional Skills:</div>
+                <ul style="list-style-type: none; padding-left: 0;">
+                    <li style="margin-bottom: 15px;">
+                        <b>Database Management:</b> Skills in managing and optimizing databases for efficient data handling.
+                    </li>
+                    <li style="margin-bottom: 15px;">
+                        <b>SQL:</b> Proficient in writing SQL queries for data retrieval and manipulation.
+                    </li>
+                    <li style="margin-bottom: 15px;">
+                        <b>Big Data Technologies:</b> Familiarity with tools like Hadoop and Spark for processing large datasets.
+                    </li>
+                    <li style="margin-bottom: 15px;">
+                        <b>Data Cleaning and Preprocessing:</b> Ensured data quality through cleaning and preprocessing techniques.
+                    </li>
+                    <li style="margin-bottom: 15px;">
+                        <b>Data Visualization:</b> Designed compelling visualizations to communicate complex data.
+                    </li>
+                    <li style="margin-bottom: 15px;">
+                        <b>Statistical Analysis:</b> Applied statistical methods for hypothesis testing and data analysis.
+                    </li>
+                    <li style="margin-bottom: 15px;">
+                        <b>Git and Version Control:</b> Used Git for version control and collaboration on projects.
+                    </li>
+                    <li style="margin-bottom: 15px;">
+                        <b>Power BI and Tableau:</b> Created interactive dashboards and reports for data visualization.
+                    </li>
+                </ul>
+                """, unsafe_allow_html=True
+            )
+
         with skill2:
-            st.write("## **how it works and its show time**")
-            new1,new2=st.columns(2)
-            with new1:
-                st.write("## This is my and second columns")
-            with new2:
-                st.write("## now it show time")
+            st.markdown(
+                """
+                <div style="font-size: 18px; color: #2C3E50; font-weight: bold;">Soft Skills:</div>
+                <ul style="list-style-type: none; padding-left: 0;">
+                    <li style="margin-bottom: 15px;">
+                        <b>Communication:</b> Clear and effective communication to convey complex data concepts to diverse audiences.
+                    </li>
+                    <li style="margin-bottom: 15px;">
+                        <b>Mentorship:</b> Experience in mentoring aspiring data scientists, providing guidance and support.
+                    </li>
+                    <li style="margin-bottom: 15px;">
+                        <b>Leadership:</b> Demonstrated leadership skills in guiding data science projects and teams.
+                    </li>
+                    <li style="margin-bottom: 15px;">
+                        <b>Problem-Solving:</b> Strong analytical and problem-solving abilities to address complex data challenges.
+                    </li>
+                    <li style="margin-bottom: 15px;">
+                        <b>Adaptability:</b> Ability to adapt to new technologies and methodologies in a dynamic data science landscape.
+                    </li>
+                    <li style="margin-bottom: 15px;">
+                        <b>Collaboration:</b> Collaborative mindset, working effectively within cross-functional teams.
+                    </li>
+                    <li style="margin-bottom: 15px;">
+                        <b>Community Building:</b> Founded and led the Needata community, fostering knowledge sharing and collaboration.
+                    </li>
+                </ul>
+                """, unsafe_allow_html=True
+            )
 
     elif page == "Projects":
         p1,p2=st.columns(2)
         with p1:
-            st.markdown("[](https://ipl-win-probability-predictor-niddydata0021.streamlit.app/?embed_options=dark_theme)")
             st.header("Projects")
             st.write("Check out some of my projects:")
-            st.write("- Project 1")
+        with p2:
+            st.write(" https://ipl-win-probability-predictor-niddydata0021.streamlit.app/?embed_options=dark_theme ")
             st.write("- Project 2")
             st.write("- Project 3")
         # Add more projects as needed
